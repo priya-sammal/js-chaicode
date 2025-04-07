543b3a10493900d4570c1069bdea2e62a8b15943
PRIYA SAMMAL <152009465+priya-sammal@users.noreply.github.com>
2025-04-07 16:38:02 +0000

    variables

--- basics/var.js
+++ basics/var.js
@@ -1,7 +1,8 @@
 const AccountId=220122300
 let Email="priyasammal3@gmail.com"
 var Password=12345
-let address;
+let address;  
+//UNDEFINED  
 City="HAldwani"
 
 console.table([AccountId,Email,Password])
